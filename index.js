a13704ec9234e2e007a7cf4c07e16f5a069d3e25
Julianna Stevenson <steve146@purdue.edu>
2016-11-13 20:53:17 -0500

    initial layout

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var express = require('express')
 var app = express()
 
+app.set('view engine', 'pug')
+
+app.use(express.static('public'));
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
+})
+
+app.get('/', function(req, res) {
+  res.render('index', {title: 'Julianna Stevenson', message: 'Julianna Stevenson'})
 })
