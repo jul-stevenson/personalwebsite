845fd11634af63c3a3be541d7ec261d54432864c
Julianna Stevenson <steve146@purdue.edu>
2016-11-20 23:06:53 -0500

    changed port

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000
 
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!')
-})
+});
 
 app.get('/', function(req, res) {
   res.render('index', {title: 'Julianna Stevenson', message: 'Julianna Stevenson'})
-})
+});
 
 app.get('/resume', function(req, res) {
   res.render('resume', {title: 'Resume'})
-})
+});
