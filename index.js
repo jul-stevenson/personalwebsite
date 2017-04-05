5bbe8eac9e72b71057659a7d92bb834cc9e62709
Julianna Stevenson <steve146@purdue.edu>
2017-04-05 14:14:04 -0400

    changed name

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
 var port = process.env.PORT || 3000
 
 app.set('view engine', 'pug');
@@ -11,9 +12,23 @@ app.listen(port, function () {
 });
 
 app.get('/', function(req, res) {
-  res.render('index', {title: 'Julianna Stevenson', message: 'Julianna Stevenson'})
+  res.render('index', {title: 'Julie Stevenson', message: 'Julianna Stevenson'})
 });
 
 app.get('/resume', function(req, res) {
   res.render('resume', {title: 'Resume'})
+});
+
+// Connect to the DB
+MongoClient.connect("mongodb://localhost:3000/", function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+  }
+  else {
+    console.log("ERROR: Couldn't connect to database.");
+  }
+
+  // Do work here with database
+
+  db.close();
 });
